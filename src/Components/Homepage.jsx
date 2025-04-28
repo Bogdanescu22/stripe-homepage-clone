a041f8fbf32aadd5865d774c481ae194603b1aff
Pantelimon Bogdan <pantelimonbogdanmihai92@gmail.com>
2025-04-28 12:16:58 +0300

    Updates

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -5,27 +5,18 @@ import Header from './Header';
 function HomePage() {
     return(
     <div className="home-page-main-div">
-    <div className="home-page-hero-div">
     <Header />
     <div className='contain-main-div'>
-    <main>
-    <div className='div12'>
-     <div className="grid-item"></div>
-     
-     <div className="grid-item"></div>
-     <div className="hero-text-item"><h1 className='hero_h1'>Financial infrastructure to grow your revenue</h1></div>
-     <div className="hero-p-item"><p>
-      Join the millions of companies that use Stripe to accept payments online and in person, embed financial services, power custom revenue models, and build a more profitable business.
-    </p></div>
-    </div>
-    <div className='div34'>
-     <div className="grid-item"></div>
-     < div className="grid-item"></div>
-    </div>
-
-
-    </main>
-    </div>
+      <main>
+         <div className='grid-div div1'>
+            <h1>Financial infrastructure to grow your revenue</h1>
+            <p>Join the millions of companies that use Stripe to accept payments online and in person, embed financial services, power custom revenue models, and build a more profitable business.  
+            </p>
+         </div>
+         <div className='grid-div div2'></div>
+         <div className='grid-div div3'></div>
+         <div className='grid-div div4'></div>
+      </main>
     </div>
     </div>
     )
